1fb582c9322f96d3a1bbc8d8a90ac9ba036b52c7
Andrey <35691656+fufylev@users.noreply.github.com>
2020-04-01 11:54:58 +0300

    Details screen was developed and tested
    Details screen was developed and tested

--- scr/context/main/MainReducer.js
+++ scr/context/main/MainReducer.js
@@ -1,11 +1,26 @@
-import { CHANGE_SCREEN } from "../types";
+import { CHANGE_SCREEN, FETCH_QUOTE, FETCH_QUOTES, FILTER_BY_SEARCH } from "../types";
 
-const handlers = {
-  [CHANGE_SCREEN]: (state, payload) => payload,
-  DEFAULT: state => state,
-};
+// const handlers = {
+//   [CHANGE_SCREEN]: (state, payload) => payload,
+//   DEFAULT: state => state,
+// };
+//
+// export const MainReducer = (state, action) => {
+//   const handler = handlers[action.type] || handlers.DEFAULT;
+//   return handler(state, action.payload);
+// };
 
 export const MainReducer = (state, action) => {
-  const handler = handlers[action.type] || handlers.DEFAULT;
-  return handler(state, action.payload);
+  switch (action.type) {
+    case CHANGE_SCREEN: {
+      return {
+        ...state,
+        quoteId: action.payload.symbol,
+        quoteData: action.payload,
+      }
+    }
+
+    default:
+      return state;
+  }
 };
